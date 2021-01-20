671faaf6d5583f6444d9a07f98238e8023b35ace
Jonathan Woollett-Light <jonathanwoollettlight@gmai.com>
2021-01-20 11:32:46 +0000

    Model versions

--- server.js
+++ server.js
@@ -70,7 +70,15 @@ app.get('/', async (req, res) => {
 
   let models = await app.locals.database.collection("models").find({public: true}).toArray();
 
-  res.render('index.ejs', { models: models });
+  const strippedModels = models.map(model => { return {
+    _id: model._id,
+    title: model.title,
+    desc: model.desc,
+    date: model.versions[model.versions.length-1].date,
+    file: model.versions[model.versions.length-1].file
+  }});
+
+  res.render('index.ejs', { models: strippedModels });
 })
 
 
@@ -81,9 +89,17 @@ app.get('/user', checkAuthenticated, async (req, res) => {
   console.log("/user")
   //console.log(req.user.models.length)
 
-  let models = await app.locals.database.collection("models").find({ _id: { $in: req.user.models } }).toArray();
+  const models = await app.locals.database.collection("models").find({ _id: { $in: req.user.models } }).toArray();
+  
+  const strippedModels = models.map(model => { return {
+    _id: model._id,
+    title: model.title,
+    desc: model.desc,
+    date: model.versions[model.versions.length-1].date,
+    file: model.versions[model.versions.length-1].file
+  }});
 
-  res.render('user.ejs', { email: req.user.email, data: req.user.data, models: models, offers: req.user.offers });
+  res.render('user.ejs', { email: req.user.email, data: req.user.data, models: strippedModels, offers: req.user.offers });
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
@@ -106,7 +122,7 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
   console.log("/register")
   //console.log(req.body)
 
-  let hash = await bcrypt.hash(req.body.password,SALT,(err,hash)).catch((err)=>{throw err});
+  const hash = await bcrypt.hash(req.body.password,SALT,(err,hash)).catch((err)=>{throw err});
 
   await app.locals.database.collection("users").insertOne({
     email: req.body.email,
@@ -149,14 +165,15 @@ app.get('/models/create', checkAuthenticated, (req, res) => {
   res.render('create-model.ejs')
 })
 
+// TODO Should this be "/models" ?
 app.post('/models/create', checkAuthenticated, async (req, res) => {
   console.log("/models/create");
   console.log(typeof req.body.public, req.body.public);
   // console.log(req.files)
-  let model = await app.locals.database.collection("models").insertOne({
+  const model = await app.locals.database.collection("models").insertOne({
     title: req.body.title,
     desc: req.body.desc,
-    file: req.files.file,
+    versions: [{file: req.files.file, date: new Date(), desc: "Initial" }],
     owners: 1,
     public: req.body.public=="on" ? true : false
   }).catch((err)=>{throw err})
@@ -169,19 +186,37 @@ app.post('/models/create', checkAuthenticated, async (req, res) => {
   res.redirect('/models/' + model.insertedId)
 })
 
-app.get('/models/:id', checkAuthenticated, (req, res) => {
+app.get('/models/:id', checkAuthenticated, async (req, res) => {
   console.log("/models/:id")
 
   if(req.user.models.some(val => val.equals(req.params.id))) {
-    app.locals.database.collection("models").findOne({_id:ObjectId(req.params.id)}, (err,result) => {
-      //console.log(result)
-      if (err) throw err
-      res.render("model.ejs",{ model: result })
-    })
+    const model = await app.locals.database.collection("models").findOne({_id:ObjectId(req.params.id)});
+    res.render("model.ejs",{ model: model })
   } else {
     res.status(403)
   }
 })
+
+app.post('/models/:id/version', checkAuthenticated, async (req, res) => {
+  console.log("/models/:id")
+  const model_id = ObjectId(req.params.id);
+
+  // Checks user has ownership of model
+  const owner = await app.locals.database.collection("users").findOne({
+    _id: req.user._id,
+    models: model_id
+  }).catch((err)=>{throw err})
+
+  if (owner != null) {
+    await app.locals.database.collection("models").updateOne(
+      { _id: model_id},
+      { $push: { versions: { file: req.files.file, date: new Date(), desc: req.body.desc }}}
+    );
+    res.redirect("/models/" + req.params.id)
+  }
+  res.status(403)
+})
+
 
 // TODO Prevents redundant offers (being offered viewership of model they can already view etc.)
 app.put('/models/:id/shareOwnership', checkAuthenticated, async (req, res) => {
@@ -190,19 +225,30 @@ app.put('/models/:id/shareOwnership', checkAuthenticated, async (req, res) => {
   //const user_id = ObjectId(req.body.id)
 
   // Checks offering user has ownership of model
-  let owner = await app.locals.database.collection("users").findOne({_id: req.user._id, models: model_id}).catch((err)=>{throw err})
+  const owner = await app.locals.database.collection("users").findOne({
+    _id: req.user._id,
+    models: model_id
+  }).catch((err)=>{throw err})
+
   //console.log("1\n",owner)
   if (owner != null) {
-    let updateResult = await app.locals.database.collection("users").findOneAndUpdate({email:req.body.email},{$addToSet:{offers: { model: model_id, type: "own", from: req.user._id }}});
+    const updateResult = await app.locals.database.collection("users").findOneAndUpdate(
+      { email: req.body.email},
+      { $addToSet: { offers: { model: model_id, type: "own", from: req.user._id }}}
+    );
+
     //console.log("2\n",updateResult)
     // Checks offered user exists
     if (updateResult.value != null) {
       //console.log("3")
-      await app.locals.database.collection("models").updateOne({_id:model_id},{$addToSet:{awaiting: { type: "own", for: updateResult.value._id }}});
+      await app.locals.database.collection("models").updateOne(
+        { _id: model_id},
+        { $addToSet: { awaiting: { type: "own", for: updateResult.value._id }}}
+      );
     }
   }
   console.log("4")
-  res.redirect("/models/"+req.params.id)
+  res.redirect("/models/" + req.params.id)
 })
 
 app.put('/models/:id/own', checkAuthenticated, async (req, res) => {
@@ -210,10 +256,12 @@ app.put('/models/:id/own', checkAuthenticated, async (req, res) => {
   const model_id = ObjectId(req.params.id);
 
   // Checks model exists and has ownership offer for user
-  let result = 
-    await app.locals.database.collection("models").findOne({_id:model_id},{awaiting: { $elemMatch: { type: "own", for: req.user._id}}})
-      .catch((err) => {throw err});
-  if (result==null) res.status(403);
+  const model = await app.locals.database.collection("models").findOne({
+    _id: model_id,
+    awaiting: { $elemMatch: { type: "own", for: req.user._id}}
+  }).catch((err) => {throw err});
+
+  if (model==null) res.status(403);
 
   await Promise.all([
     app.locals.database.collection("models").updateOne(
@@ -241,12 +289,19 @@ app.delete('/models/:id/disown', checkAuthenticated, async (req, res) => {
   const model_id = ObjectId(req.params.id);
 
   // Removes user ownership
-  let user = await app.locals.database.collection("users").updateOne({_id:req.user._id}, {$pull:{models:model_id}}).catch((err)=>{throw err});
+  const user = await app.locals.database.collection("users").updateOne(
+    { _id: req.user._id },
+    { $pull: { models: model_id } }
+  ).catch((err)=>{throw err});
 
   // Checks update was made in removing user ownership (in affect checks user did own model)
   if (user.modifiedCount == 1) {
     // Decrease owner count
-    let model = await app.locals.database.collection("models").findOneAndUpdate({_id: model_id}, { $inc: {owners: -1}}).catch((err)=>{throw err});
+    const model = await app.locals.database.collection("models").findOneAndUpdate(
+      {_id: model_id},
+      { $inc: {owners: -1}}
+    ).catch((err)=>{throw err});
+
     // If owners equalled 1 before the change, it equals 0 now, therefore delete
     if (model.value.owners == 1){
       await app.locals.database.collection("models").deleteOne(({_id: model_id})).catch((err)=>{throw err});
